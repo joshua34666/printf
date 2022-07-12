47b4cb16bd9aa446098ecdfb79932341950e19c1
KiraLee2001 <olonisayeprecious@gmail.com>
2022-07-12 16:35:16 -0700

    checking for number of identifiers

--- ev_print_func.c
+++ ev_print_func.c
@@ -1,9 +1,9 @@
 #include "main.h"
 /**
- * ev_print_func - returns the amount of identifiers.
+ * ev_print_func - returns the number of identifiers.
  * @s: argument indentifier
  * @index: index of argument identifier.
- * Return: amount of identifiers.
+ * Return: number of identifiers.
  */
 int ev_print_func(const char *s, int index)
 {
