d6405cbfe2e827a0d45389b250660f731571ad21
KiraLee2001 <olonisayeprecious@gmail.com>
2022-07-12 16:43:34 -0700

    storing a character to stdout

--- fill_hex_array.c
+++ fill_hex_array.c
@@ -2,8 +2,8 @@
 /**
  * fill_hex_array - writes the character c to stdout
  *
- * @bnr: array where is stored the binary.
- * @hex: array where is stored the hexadecimal.
+ * @bnr: array which stores the binary.
+ * @hex: array which is stores the hexadecimal.
  * @isupp: integer that determines if the hexadecimal array is
  * in uppercase or lowercase letter.
  * @limit: size of hex
