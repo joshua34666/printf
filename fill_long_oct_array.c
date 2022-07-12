9698a9586fcb085a0176d7971aeae9158ca5bf66
KiraLee2001 <olonisayeprecious@gmail.com>
2022-07-12 16:46:06 -0700

    calculating a long octal number

--- fill_long_oct_array.c
+++ fill_long_oct_array.c
@@ -2,8 +2,8 @@
 /**
  * fill_long_oct_array - calculates a long octal number
  *
- * @bnr: array where is stored the binary.
- * @oct: array where is stored the octal.
+ * @bnr: array which stores the binary.
+ * @oct: array which stores the octal.
  *
  * Return: binary array.
  */
