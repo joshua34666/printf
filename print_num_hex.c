b94afe99253aa78d05fc7f5f1dd5f21892e35d7e
KiraLee2001 <olonisayeprecious@gmail.com>
2022-07-12 18:31:15 -0700

    print hexadecimal

--- print_num_hex.c
+++ print_num_hex.c
@@ -2,7 +2,7 @@
 /**
  * prinnhex - print number in hex begining with zero
  * @arguments: input string
- * @buf: buffer pointer
+ * @buf: pointer to buffer
  * @ibuf: index for buffer pointer
  * Return: number of chars printed
  */
