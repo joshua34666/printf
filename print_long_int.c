41cb025a7ac60b9cbcbb240383d2cb5449d01c69
KiraLee2001 <olonisayeprecious@gmail.com>
2022-07-12 18:20:49 -0700

    print a long integer

--- print_long_int.c
+++ print_long_int.c
@@ -3,7 +3,7 @@
  * prinlint - prints a long integer
  * @arguments: input string
  * @buf: buffer pointer
- * @ibuf: index for buffer pointer
+ * @ibuf: index for pointer to buffer
  * Return: number of chars printed.
  */
 int prinlint(va_list arguments, char *buf, unsigned int ibuf)
